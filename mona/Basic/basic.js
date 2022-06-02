71a9ed88239b6c5d4339235883e2ce30dbc149b0
MonaFahham <monafahham85@gmail.com>
2022-06-02 12:42:15 +0200

    basic2

--- mona/Basic/basic.js
+++ mona/Basic/basic.js
@@ -1,61 +1,111 @@
 // Ex 1
-const car = {
-    name: "Mercedes-Benz ",
-    generation: "first",
-    model: "C-Class",
-    door: "4",
-    color: "white",
-    automatic: "yes",
-    year: "2020",
-    carBrand: function() {
-        return this.name + this.model;
-    },
-};
-const animal = {
-    name: "Snowflake",
-    gender: "girl",
-    species: "Chow Chow",
-    food: "Blue Buffalo Lamb & Brown Rice",
-    color: "white",
-    weight: "30",
-    birthYear: "2021",
-    LifeExpectancy: "10",
-    animalType: function() {
-        return this.color + this.species;
-    },
-};
-const person = {
-    firstName: "Marry",
-    lastName: "Doe",
-    gender: "female",
-    birthYear: "2000",
-    job: "Web developer",
-    friends: ["sarah", "Suzy", "Maria"],
-    hasDriversLicence: true,
-    calcAge: function() {
-        this.age = 2037 - this.birthYear;
-        return this.age;
-    },
-};
-console.log(person.calcAge());
-document.getElementById("age").innerHTML = `<h2>${
-  person.firstName
-}, you are ${person.calcAge()} years old</h2>`;
+// const car = {
+//     name: "Mercedes-Benz ",
+//     generation: "first",
+//     model: "C-Class",
+//     door: "4",
+//     color: "white",
+//     automatic: "yes",
+//     year: "2020",
+//     carBrand: function() {
+//         return this.name + this.model;
+//     },
+// };
+// const animal = {
+//     name: "Snowflake",
+//     gender: "girl",
+//     species: "Chow Chow",
+//     food: "Blue Buffalo Lamb & Brown Rice",
+//     color: "white",
+//     weight: "30",
+//     birthYear: "2021",
+//     LifeExpectancy: "10",
+//     animalType: function() {
+//         return this.color + this.species;
+//     },
+// };
+// const person = {
+//     firstName: "Marry",
+//     lastName: "Doe",
+//     gender: "female",
+//     birthYear: "2000",
+//     job: "Web developer",
+//     friends: ["sarah", "Suzy", "Maria"],
+//     hasDriversLicence: true,
+//     calcAge: function() {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+// };
+// console.log(person.calcAge());
+// document.getElementById("age").innerHTML = `<h2>${
+//   person.firstName
+// }, you are ${person.calcAge()} years old</h2>`;
 
-console.log(car.carBrand());
-document.getElementById(
-    "car"
-).innerHTML = `<h3> Your car is ${car.carBrand()} </h3>`;
+// console.log(car.carBrand());
+// document.getElementById(
+//     "car"
+// ).innerHTML = `<h3> Your car is ${car.carBrand()} </h3>`;
 
-console.log(animal.animalType());
-document.getElementById(
-    "animal"
-).innerHTML = `<h4> You have a ${animal.animalType()} and her name is ${
-  animal.name
-}</h4>`;
+// console.log(animal.animalType());
+// document.getElementById(
+//     "animal"
+// ).innerHTML = `<h4> You have a ${animal.animalType()} and her name is ${
+//   animal.name
+// }</h4>`;
+
+// Ex 1
+let theme = {
+    car: [{
+        name: "Mercedes-Benz ",
+        generation: "first",
+        model: "C-Class",
+        door: "4",
+        color: "white",
+        automatic: "yes",
+        year: "2020",
+        carBrand: function() {
+            return this.name + this.model;
+        },
+    }, ],
+    animal: [{
+        name: "Snowflake",
+        gender: "girl",
+        species: "Chow Chow",
+        food: "Blue Buffalo Lamb & Brown Rice",
+        color: "white",
+        weight: "30",
+        birthYear: "2021",
+        LifeExpectancy: "10",
+        animalType: function() {
+            return this.color + this.species;
+        },
+    }, ],
+    person: [{
+        firstName: "Marry",
+        lastName: "Doe",
+        gender: "female",
+        birthYear: "2000",
+        job: "Web developer",
+        friends: ["sarah", "Suzy", "Maria"],
+        hasDriversLicence: true,
+        calcAge: function() {
+            this.age = 2037 - this.birthYear;
+            return this.age;
+        },
+    }, ],
+};
+
+let result = document.getElementById("theme");
+
+result.innerHTML = `<h2> Your Information </h2>`;
+for (i = 0; i < theme.car.length; i++) {
+    result.innerHTML += `<div><h3>Your car: </h3>${theme.car[i].name}</div>
+    <div><h3>Model: </h3>${theme.car[i].name}</div>
+    <div><h3>Generation: </h3>${theme.car[i].generation} </div>`;
+}
 
 // Ex 2
-// My favorite sandwich is a Hamburger which has approximately 260 calories, along with it I enjoy eating Large French Fries which have about 570 calories.
 var sandwiches = `[{ 
     "sandwich": "hamburger",
      "calories": "260" 
